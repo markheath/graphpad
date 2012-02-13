32a69ca847a3edde4d0f5f3fc4424c5c0f849587
Mark Heath <mark.heath@gmail.com>
2012-02-13 13:07:05 +0000

    node positioning

--- GraphPad/MainWindow.xaml.cs
+++ GraphPad/MainWindow.xaml.cs
@@ -37,6 +37,8 @@ namespace GraphPad
             var tokens = Tokenizer.Tokenize(graphText.Text);
             graphCanvas.Children.Clear();
             nodes.Clear();
+            double top = 10;
+            double left = 10;
             foreach (var token in tokens)
             {
                 if (token == ">")
@@ -45,16 +47,23 @@ namespace GraphPad
                 }
                 else if (token.Trim().Length > 0)
                 {
-                    Ellipse node = new Ellipse();
-                    node.Width = 50;
-                    node.Height = 50;
-                    node.SetValue(Canvas.LeftProperty, 50.0);
-                    node.SetValue(Canvas.TopProperty, 50.0);
-                    node.Stroke = Brushes.Black;
-                    node.StrokeThickness = 2;
+                    Ellipse node = CreateNode(left, top);
+                    left += node.Width + 10;
                     graphCanvas.Children.Add(node);
                 }
             }
+        }
+
+        private static Ellipse CreateNode(double left, double top)
+        {
+            Ellipse node = new Ellipse();
+            node.Width = 40;
+            node.Height = 40;
+            node.SetValue(Canvas.LeftProperty, left);
+            node.SetValue(Canvas.TopProperty, top);
+            node.Stroke = Brushes.Black;
+            node.StrokeThickness = 2;
+            return node;
         }
     }
 }
