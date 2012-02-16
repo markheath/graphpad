85a1722fc4b0d92830a6ec3fb58666aef21e3d79
Mark Heath <mark.heath@gmail.com>
2012-02-16 16:07:45 +0000

    tokenizer improvements

--- GraphPad.Tests/TokenizerTests.cs
+++ GraphPad.Tests/TokenizerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using GraphPad.Logic;
 
 namespace GraphPad.Tests
 {
@@ -12,8 +13,24 @@ namespace GraphPad.Tests
         [Test]
         public void CanParseBasicDirectedEdge()
         {
-            string[] tokens = Tokenizer.Tokenize("a>b");
+            var tokens = Tokenizer.Tokenize("a>b");
             string[] expected = { "a", ">", "b" };
+            Assert.AreEqual(expected, tokens);
+        }
+        
+        [Test]
+        public void IgnoreWhitespace()
+        {
+            var tokens = Tokenizer.Tokenize("a > b");
+            string[] expected = { "a", ">", "b" };
+            Assert.AreEqual(expected, tokens);
+        }
+
+        [Test]
+        public void CanParseUndirectedEdge()
+        {
+            var tokens = Tokenizer.Tokenize("a-b");
+            string[] expected = { "a", "-", "b" };
             Assert.AreEqual(expected, tokens);
         }
     }
---
+++ graphpad/logic/tokenizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GraphPad.Logic
+{
+    class Tokenizer
+    {
+        // helpful article http://en.csharp-online.net/CSharp_Regular_Expression_Recipes%E2%80%94A_Better_Tokenizer
+
+        public static IEnumerable<string> Tokenize(string graphMarkup)
+        {
+            Regex regularExpression = new Regex(@"([\-\<\>\r\n])");
+            return (regularExpression.Split(graphMarkup).
+                Select(x => x.Trim()).
+                Where(x => !String.IsNullOrWhiteSpace(x)));
+        }
+
+    }
+
+}
